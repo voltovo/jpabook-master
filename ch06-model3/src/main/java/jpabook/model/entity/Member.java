e630029977224c9f1b473367c96be0b6d8173f81
sumanChoi <voltovo@naver.com>
2022-07-04 20:32:56 +0900

    FEAT: member entity complete

--- ch06-model3/src/main/java/jpabook/model/entity/Member.java
+++ ch06-model3/src/main/java/jpabook/model/entity/Member.java
@@ -8,7 +8,77 @@ import java.util.List;
  * Created by holyeye on 2014. 3. 11..
  */
 
-
+@Entity
 public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+
+    private String city;
+
+    private String street;
+
+    private String zipcode;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<Order>();
+
+    public Long getId() {
+        return this.id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getCity() {
+        return this.city;
+    }
+    
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+    public String getStreet() {
+        return this.street;
+    }
+    
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    
+    public String getZipcode() {
+        return this.zipcode;
+    }
+    
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+    
+    public List<Order> getOrders() {
+        return this.orders;
+    }
+    
+    // Member -> Order 일대다 단방향 관계여서 무한루프 체크 X ??
+    // public void addOrder(Order order) {
+    //     this.orders.add(order);
+    //     if(order.getMember() != this){
+    //         order.setMember(this);
+    //     }
+    // }
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 
 }
--- ch06-model3/src/main/java/jpabook/model/entity/Order.java
+++ ch06-model3/src/main/java/jpabook/model/entity/Order.java
@@ -1,6 +1,8 @@
 package jpabook.model.entity;
 
 import javax.persistence.*;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -8,7 +10,83 @@ import java.util.List;
 /**
  * Created by holyeye on 2014. 3. 11..
  */
-
+@Entity
 public class Order {
 
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany
+    private List<OrderItem> orderItems = new ArrayList<OrderItem>();
+
+    @OneToOne
+    private Delivery delivery;
+
+    private LocalDate orderDate;
+
+    private OrderStatus status;
+
+    public void setMember(Member member){
+        //기존 멤버와 관례를 제거
+        if(this.member != null){
+            this.member.getOrders().remove(this);
+        }
+
+        //무한 루프에 빠지지 않도록 체크
+        if(!member.getOrders().contains(this)){
+            this.member = member;
+            member.getOrders().add(this);
+        }
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return this.member;
+    }
+
+
+    public List<OrderItem> getOrderItems() {
+        return this.orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public Delivery getDelivery() {
+        return this.delivery;
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+    }
+
+    public LocalDate getOrderDate() {
+        return this.orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+    
 }
